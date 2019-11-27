2b82b9406b7267db0889b042db2f1d3ea9d90765
Christine Chepkoech <christinechep970@gmail.com>
2019-11-27 10:31:33 +0300

    Initial commit

---
+++ app/src/main/java/myapp/user/example/com/sikika2/MainActivity.java
@@ -0,0 +1,14 @@
+package myapp.user.example.com.sikika2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
